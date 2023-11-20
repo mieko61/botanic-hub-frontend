24fbd3689e07ef01730db22fa934a0242ffe6f36
Mieko <miekotominaga@gmail.com>
2023-11-19 22:02:17 -0500

    add initial styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,15 @@
 import "./Header.scss";
+import logo from "../../assets/images/logo.svg";
+import heartIcon from "../../assets/images/icons/heart.svg";
+import settingsIcon from "../../assets/images/icons/settings.svg";
 
 const Header = () => {
   return (
     <header className="header">
-      <h1 className="header__logo">BOTANIC HUB</h1>
+      <img src={logo} alt="logo" className="header__logo" />
       <div className="header__nav">
-        <img src="/" alt="favorites icon" />
-        <img src="/" alt="settings icon" />
+        <img src={heartIcon} alt="favorites icon" className="header-icon" />
+        <img src={settingsIcon} alt="settings icon" className="header-icon" />
       </div>
     </header>
   );
